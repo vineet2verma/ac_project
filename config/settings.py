dcfdd477d3573655719b4457130ad278556cd362
vineet2verma <63637317+vineet2verma@users.noreply.github.com>
2025-12-28 23:12:07 +0530

    Revert "Main"

--- config/settings.py
+++ config/settings.py
@@ -127,6 +127,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
